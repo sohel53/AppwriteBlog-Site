b4afdf1e9d1a18bdfd9a213823acd75efe84e334
Mahebub Maniyar <maniyarmahebub0148@gmail.com>
2024-02-09 02:53:50 +0530

    Last commit

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -5,16 +5,16 @@ function Select({
     label,
     className,
     ...props
-},ref) {
+}, ref) {
     const id = useId()
   return (
     <div className='w-full'>
-        {label && <label htmlFor={id} className=''>
+        {label && <label htmlFor={id} className=''></label>}
         <select
-            {...props}
-            id={id}
-            ref={ref}
-            className = {`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+        {...props}
+        id={id}
+        ref={ref}
+        className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
         >
             {options?.map((option) => (
                 <option key={option} value={option}>
@@ -22,9 +22,6 @@ function Select({
                 </option>
             ))}
         </select>
-
-        </label>}
-
     </div>
   )
 }
